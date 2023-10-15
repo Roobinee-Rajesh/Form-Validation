bfb4ec4960e80b983d754784e67c5ab47417faf0
Roobinee-Rajesh <roobinee.rajesh@kumaran.com>
2023-10-15 19:39:27 +0530

    first commit

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { Form } from 'src/app/model/form';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent {
+  name: string = '';
+  email: string = '';
+  phoneNumber: string = '';
+  editId: number = 0;
+  btnName:string="Add";
+
+  list: Form[] = [
+    {
+      id: 1,
+      name: 'Roobinee',
+      email: 'roobineer.2002@gmail.com',
+      phoneNumber: 6380749031,
+    },
+    {
+      id: 2,
+      name: 'Dharun',
+      email: 'dharun@gmail.com',
+      phoneNumber: 9877749031,
+    },
+    {
+      id: 3,
+      name: 'Barath',
+      email: 'barath@gmail.com',
+      phoneNumber: 86562749031,
+    }
+  ];
+
+  getRandomNumber = (max = 1000): number => {
+    return Math.floor(Math.random() * max);
+  };
+
+  addList = (): void => {
+    if (this.editId === 0) {
+      if (this.name !== '' && this.email !== '' && this.phoneNumber !== '') {
+        this.list.push({
+          id: this.getRandomNumber(),
+          name: this.name,
+          email: this.email,
+          phoneNumber: parseInt(this.phoneNumber),
+        });
+      }
+    } else {
+      let index: number = this.list.findIndex((c) => c.id === this.editId);
+      let item: Form = this.list.find((item) => item.id === this.editId)!;
+      this.list[index] = {
+        id: this.editId,
+        name: this.name,
+        email: this.email,
+        phoneNumber: parseInt(this.phoneNumber),
+      };
+    }
+    this.name = '';
+    this.email = '';
+    this.phoneNumber = '';
+    this.btnName="Add";
+    this.editId = 0;
+  };
+
+  deleteList = (id: number) => {
+    this.list = this.list.filter((item) => item.id !== id);
+    return this.list;
+  };
+
+  editList = (id: number, name: string, email: string, phoneNumber: number) => {
+    this.editId = id;
+    this.name = name;
+    this.email = email;
+    this.phoneNumber = phoneNumber.toString();
+    this.btnName="Edit";
+  };
+}
